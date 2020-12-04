f322736f8193ea9c3a30834a5208db3f8273dc07
Dorkwoodgames <dorkwoodgames@gmail.com>
2020-12-04 13:26:45 -0800

    Add project files.

---
+++ ERQueue.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedicalQueue_Boyd
+{
+    class ERQueue
+    {
+        /* I used a linked list for this project since I thought it would prove the most useful for a short
+         * list where I could add patients from the back in an order without a lot of searching
+         * the way I added the new patients was from the back so I could keep priority if someone has the same priority
+         * I could add them behind the current patient, or infront if they had the highest priority of the highest priority
+         * queue.
+         * I did not make a patient class becuse I don't think the project called for it becuse I used a nexted Node class
+         * making a patient class would only add extra work for little gain the patient info is stored in the node class and can
+         * is only read only.
+         * 
+         * 
+         * 
+         */
+        internal class Node
+        {
+            internal Node next;
+            internal Node last;
+            private string name;
+            public string Name
+            {
+                get { return name; }
+            }
+
+            private int priority;
+            public int Priority
+            {
+                get { return priority; }
+            }
+            internal Node(string _Name, int _Priority)
+            {
+                name = _Name;
+                priority = _Priority;
+                next = null;
+                last = null;
+
+            }    
+        }
+
+        int currentQue = 0;
+        Node head;
+        Node tail;
+
+        public int Enqueue(string Name, int Priority)
+        {
+            currentQue++;//for knowing how many people are in the list
+            Node newNode = new Node(Name, Priority);
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+                return currentQue;
+            }
+            Node tempEnd = tail;
+            while (tempEnd != null)
+            {
+                // first person
+                if (newNode.Priority < tempEnd.Priority && tempEnd.next == null)
+                {
+                    tempEnd.next = newNode;
+                    newNode.last = tempEnd;
+                    head = newNode;
+                    return currentQue;
+                }
+                //end of que
+                else if (newNode.Priority >= tempEnd.Priority && tempEnd.last == null)
+                {
+                    tempEnd.last = newNode;
+                    newNode.next = tempEnd;
+                    tail = newNode;
+                    return currentQue;
+                }
+                
+                else if (newNode.Priority >= tempEnd.Priority)
+                {
+                    tempEnd.last.next = newNode;
+                    newNode.last = tempEnd.last;
+                    tempEnd.last = newNode;
+                    newNode.next = tempEnd;
+
+                    return currentQue;
+                }
+                else
+                {
+                    tempEnd = tempEnd.next;
+                }
+
+            }
+            
+                return -1;
+            
+        }
+        public string Dequeue()
+        {
+            if (head == null)
+            {
+                return "no people in que";    
+            }
+            string removedPerson = head.Name + " " + head.Priority.ToString();
+            head = head.last;
+            if (currentQue == 1)
+            {
+                head = null;
+                tail = null;
+            }
+            else
+            {
+                head.next = null;
+            }
+            currentQue--;
+            return removedPerson;
+        }
+        public override string ToString()
+        {
+            StringBuilder patientsInfo = new StringBuilder();
+            Node ListValue = head;
+            int postionInQueue = 0;
+            while (ListValue != null)
+            {
+                postionInQueue++;
+                patientsInfo.Append("Patiens name is "+ListValue.Name + " their priority is " + ListValue.Priority +" their postion in the queue is "+ postionInQueue + "\n");
+                ListValue = ListValue.last;
+            }
+            
+            //string builder that adds all items in the que both name and priority
+            return patientsInfo.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MedicalQueue_Boyd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ERQueue que = new ERQueue();
+            bool Looper = true;
+            while(Looper == true)
+            {
+                Console.WriteLine(" (A)dd Patient  (P)rocess Current Patient  (L)ist All in Queue  (Q)uit");
+                ConsoleKey userKey = Console.ReadKey().Key;
+                Console.Clear();
+                if (userKey == ConsoleKey.A)
+                {
+                    Console.WriteLine("add the name of your person");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("add their priority");
+                    int priority =Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine( "current people in queue "+ que.Enqueue(name, priority));
+ 
+                }
+                else if (userKey == ConsoleKey.P)
+                {
+                   
+                    Console.WriteLine("this person was removed "+que.Dequeue());
+                }
+                else if (userKey == ConsoleKey.L)
+                {
+
+                    Console.WriteLine("here is the list of all Patients in line \n"+que.ToString());
+                }
+                else if (userKey == ConsoleKey.Q)
+                {
+                    Console.WriteLine("You have quit the program.");
+                        Looper = false;
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("you didn't enter the right key.");
+                }
+            }
+        }
+    }
+}
